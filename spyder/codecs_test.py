c23935807ba5ab53850e944e613d5f8d05477347
DAT-ASSET-36 <rajesh.samui@digitalaptech.com>
2019-08-05 20:19:04 +0530

    python cookbook

--- spyder/codecs_test.py
+++ spyder/codecs_test.py
@@ -4,3 +4,113 @@ Created on Tue July 25 14:31:24 2019
 
 @author: Rajesh Samui
 """
+import codecs
+
+class CodecsTest:
+    """
+    Most standard codecs are text encodings, which encode text to bytes, but 
+    there are also codecs provided that encode text to text, and bytes to 
+    bytes. Custom codecs may encode and decode between arbitrary types, but 
+    some module features are restricted to use specifically with text 
+    encodings, or with codecs that encode to bytes.
+    """
+    def __init__(self):
+        pass
+    
+    def codecs_encode_test(self):
+        """
+        Encodes obj using the codec registered for encoding.
+
+        Errors may be given to set the desired error handling scheme. The 
+        default error handler is 'strict' meaning that encoding errors raise 
+        ValueError (or a more codec specific subclass, such as 
+        UnicodeEncodeError). Refer to Codec Base Classes for more information 
+        on codec error handling.
+        """
+        string = 'This is a test string.'
+        e_string = codecs.encode(string, encoding='utf-8')
+        print('e_string =', e_string)
+        return
+        
+    def codecs_decode_test(self):
+        """
+        Decodes obj using the codec registered for encoding.
+
+        Errors may be given to set the desired error handling scheme. The 
+        default error handler is 'strict' meaning that decoding errors raise 
+        ValueError (or a more codec specific subclass, such as 
+        UnicodeDecodeError). Refer to Codec Base Classes for more information 
+        on codec error handling.
+        """
+        string = 'This is a test string.'
+        e_string = codecs.encode(string, encoding='utf-8')
+        print('e_string =', e_string)
+        d_string = codecs.decode(e_string, encoding='utf-8')
+        print('d_string =', d_string)
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    ct = CodecsTest()
+    print(ct.__doc__)
+    
+    print("\n# codecs.encode(obj, encoding='utf-8', errors='strict')")
+    print(ct.codecs_encode_test.__doc__)
+    ct.codecs_encode_test()
+    
+    print("\n# codecs.decode(obj, encoding='utf-8', errors='strict')")
+    print(ct.codecs_decode_test.__doc__)
+    ct.codecs_decode_test()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- spyder/python_cookbook.py
+++ spyder/python_cookbook.py
@@ -4,9 +4,10 @@ Created on Tue July 11 14:31:24 2019
 
 @author: Rajesh Samui
 """
-from collections import deque
+from collections import deque, defaultdict, OrderedDict
 import heapq
 from pprint import pprint
+import json
 
 class PythonCookBook:
     """
@@ -142,15 +143,247 @@ class PythonCookBook:
             {'name': 'YHOO', 'shares': 45, 'price': 16.35},
             {'name': 'ACME', 'shares': 75, 'price': 115.65}
         ]
+        pprint(portfolio)
+        print()
         cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
         pprint(cheap)
+        print()
         expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
         pprint(expensive)
         
-
-
-
-
+        print('*'*50)
+        print('nums =', nums)
+        heapq.heapify(nums)
+        print('nums =', nums)
+        print('heapq.heappop(nums):', heapq.heappop(nums))
+        print('nums =', nums)
+        print('heapq.heappop(nums):', heapq.heappop(nums))
+        print('nums =', nums)
+        print('heapq.heappop(nums):', heapq.heappop(nums))
+        print('nums =', nums)
+        print('heapq.heappop(nums):', heapq.heappop(nums))
+        print('nums =', nums)
+        print('heapq.heappop(nums):', heapq.heappop(nums))
+        print('''
+        The most important feature of a heap is that heap[0] is always the 
+        smallest item. Moreover, subsequent items can be easily found using the
+        heapq.heappop() method, which pops off the first item and replaces it 
+        with the next smallest item (an operation that requires O(log N) 
+        operations where N is the size of the heap). For example, to find the
+        three smallest items, you would do this.
+        ''')
+        print('\n# 1.5. Implementing a Priority Queue')
+        print('''
+        Problem:
+            You want to implement a queue that sorts items by a given priority 
+            and always returns the item with the highest priority on each pop 
+            operation.
+        ''')
+        class PriorityQueue:
+            def __init__(self):
+                self._queue = []
+                self._index = 0
+            def push(self, item, priority):
+                heapq.heappush(self._queue, (-priority, self._index, item))
+                self._index += 1
+            def pop(self):
+                return heapq.heappop(self._queue)[-1]
+            
+        class Item:
+            def __init__(self, name):
+                self.name = name
+            def __repr__(self):
+                return 'Item({!r})'.format(self.name)
+        
+        q = PriorityQueue()
+        print('PriorityQueue():', PriorityQueue())
+        q.push(Item('foo'), 1)
+        q.push(Item('bar'), 5)
+        q.push(Item('spam'), 4)
+        q.push(Item('grok'), 1)
+        print('q.pop():', q.pop())
+        print('q.pop():', q.pop())
+        print('q.pop():', q.pop())
+        print('q.pop():', q.pop())
+        
+        print('*'*50)
+        a = Item('foo')
+        b = Item('bar')
+        print(a, b, 'a < b', sep='\n')
+        try:
+            a < b
+        except Exception as e:
+            print(type(e), e)
+        
+        print('*'*50)
+        a = (1, Item('foo'))
+        b = (5, Item('bar'))
+        print(a, b, sep='\n')
+        print('a < b:', a < b)
+        c = (1, Item('grok'))
+        try:
+            print('a < c:', a < c)
+        except Exception as e:
+            print(type(e), e)
+            
+        print('*'*50)
+        a = (1, 0, Item('foo'))
+        b = (5, 1, Item('bar'))
+        c = (1, 2, Item('grok'))
+        print(a, b, c, sep='\n')
+        print('a < b', a<b)
+        print('a < c', a<c)
+        
+        print('\n# 1.6. Mapping Keys to Multiple Values in a Dictionary')
+        print('''
+        Problem:
+            You want to make a dictionary that maps keys to more than one value
+            (a so-called “multidict”).
+        ''')
+        d = defaultdict(list)
+        print('defaultdict(list):', d)
+        d['a'].append(1)
+        d['a'].append(2)
+        d['b'].append(4)
+        print('defaultdict(list):', d)
+        
+        print('*'*50)
+        
+        d = defaultdict(set)
+        print('defaultdict(set):', d)
+        d['a'].add(1)
+        d['a'].add(2)
+        d['b'].add(4)
+        print('defaultdict(set):', d)
+        
+        print('*'*50)
+        d = {}    # A regular dictionary
+        print('d =', d)
+        d.setdefault('a', []).append(1)
+        d.setdefault('a', []).append(2)
+        d.setdefault('b', []).append(4)
+        print('d =', d)
+        
+        print('*'*50)
+        
+        print('\n# 1.7. Keeping Dictionaries in Order')
+        print('''
+        Problem:
+            You want to create a dictionary, and you also want to control the 
+            order of items when iterating or serializing.
+        ''')
+        d = OrderedDict()
+        print('d =', d)
+        d['foo'] = 1
+        d['bar'] = 2
+        d['spam'] = 3
+        d['grok'] = 4
+        print('d =', d)
+        [print(k, v, sep=':') for k, v in d.items()]
+        print('''
+        An OrderedDict can be particularly useful when you want to build a 
+        mapping that you may want to later serialize or encode into a different
+        format. For example, if you want to precisely control the order of 
+        fields appearing in a JSON encoding, first building the data in an 
+        OrderedDict will do the trick.
+        ''')
+        print('json.dumps(d):', json.dumps(d))
+        print('''
+        An OrderedDict internally maintains a doubly linked list that orders 
+        the keys according to insertion order. When a new item is first 
+        inserted, it is placed at the end of this list. Subsequent reassignment
+        of an existing key doesn’t change the order.
+        ''')
+        
+        print('\n# 1.8. Calculating with Dictionaries')
+        print('''
+        Problem:
+            You want to perform various calculations (e.g., minimum value, 
+            maximum value, sort‐ing, etc.) on a dictionary of data.
+        ''')
+        prices = {
+            'ACME': 45.23,
+            'AAPL': 612.78,
+            'IBM': 205.55,
+            'HPQ': 37.20,
+            'FB': 10.75
+        }
+        print('prices =',prices)
+        print('min(zip(prices.values(), prices.keys())):', 
+              min(zip(prices.values(), prices.keys())))
+        print('max(zip(prices.values(), prices.keys())):', 
+              max(zip(prices.values(), prices.keys())))
+        pprint(sorted(zip(prices.values(), prices.keys())))
+        print('''
+        When doing these calculations, be aware that zip() creates an iterator 
+        that can only be consumed once.
+        ''')
+        prices_and_names = zip(prices.values(), prices.keys())
+        print(min(prices_and_names)) # OK
+        try:
+            print(max(prices_and_names)) # ValueError: max() arg is an empty sequence
+        except Exception as e:
+            print(type(e), e)
+            
+        print('min(prices):', min(prices))
+        print('max(prices):', max(prices))
+        print('min(prices.values()):', min(prices.values()))
+        print('max(prices.values()):', max(prices.values()))
+        
+        print('min(prices, key=lambda k: prices[k]):', 
+                   min(prices, key=lambda k: prices[k]))
+        print('max(prices, key=lambda k: prices[k]):', 
+                   max(prices, key=lambda k: prices[k]))
+        min_value = prices[min(prices, key=lambda k: prices[k])]
+        print('min_value:', min_value)
+        print('''
+        It should be noted that in calculations involving (value, key) pairs, 
+        the key will be used to determine the result in instances where 
+        multiple entries happen to have the same value. For instance, in 
+        calculations such as min() and max() , the entry with the smallest or 
+        largest key will be returned if there happen to be duplicate values.
+        ''')
+        prices = { 'AAA' : 45.23, 'ZZZ': 45.23 }
+        pprint(prices)
+        print('min(zip(prices.values(), prices.keys())):', 
+              min(zip(prices.values(), prices.keys())))
+        print('max(zip(prices.values(), prices.keys())):', 
+              max(zip(prices.values(), prices.keys())))
+        
+        print('\n# 1.9. Finding Commonalities in Two Dictionaries')
+        print('''
+        Problem:
+            You have two dictionaries and want to find out what they might have
+            in common (same keys, same values, etc.).
+        ''')
+        a = {'x' : 1, 'y' : 2, 'z' : 3}
+        b = {'w' : 10, 'x' : 11, 'y' : 2}
+        print('a.keys() & b.keys():', a.keys() & b.keys())
+        print('a.keys() | b.keys():', a.keys() | b.keys())
+        print('a.keys() - b.keys():', a.keys() - b.keys())
+        print('a.items() & b.items():', a.items() & b.items())
+        c = {key:a[key] for key in a.keys() - {'z', 'w'}}
+        pprint(c)
+        
+        print('\n# 1.10. Removing Duplicates from a Sequence while Maintaining Order')
+        print('''
+        Problem:
+            You want to eliminate the duplicate values in a sequence, but 
+            preserve the order of the remaining items.
+        ''')
+        def dedupe(items):
+            seen = set()
+            for item in items:
+                if item not in seen:
+                    yield item
+                    seen.add(item)
+                    
+        a = [1, 5, 2, 1, 9, 1, 5, 10]
+        print(list(dedupe(a)))
+        
+        
+        
+        
 
 if __name__ == '__main__':
     pcb = PythonCookBook()
